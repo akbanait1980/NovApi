fa1eca1437db1b4296edf0670bd06e70be5da897
Amol Banait <amolbigdata@gmail.com>
2018-11-26 21:21:30 -0500

    First Commit

---
+++ NovApi/src/main/java/com/learn/restapi/nov/NovApi/NovApiApplication.java
@@ -0,0 +1,12 @@
+package com.learn.restapi.nov.NovApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovApiApplication.class, args);
+	}
+}
---
+++ NovApi/src/test/java/com/learn/restapi/nov/NovApi/NovApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.restapi.nov.NovApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NovApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
